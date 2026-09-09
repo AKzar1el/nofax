5cc473dd176a987f64a9c77702ac6ff9367c90a6
Tommy <104433268+AKzar1el@users.noreply.github.com>
2026-09-09 21:11:17 +0200

    test: type Cloudflare worker bindings

--- worker/test/env.d.ts
+++ worker/test/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="@cloudflare/workers-types" />
+
+import type { Env as NofaxEnv } from "../src/env";
+
+declare module "cloudflare:workers" {
+  interface ProvidedEnv extends NofaxEnv {}
+}
